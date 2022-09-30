8f843b4074a2d47ec25ba0f3bb4e4e03a7fcc3f0
Emily <easchoettler@gmail.com>
2022-09-30 10:05:19 -0400

    Update createGrid() to use flex-wrap for grid rows instead of row containers

--- myScript.js
+++ myScript.js
@@ -1,13 +1,30 @@
+//GRID SETUP
+
 const gridContainer = document.querySelector("#grid-container");
 
-for (i = 1; i < 17; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    gridContainer.appendChild(gridRow);
-
-    for (j = 1; j < 17; j++) {
+function createGrid(num) {
+    for (i = 1; i <= (num * num); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        gridRow.appendChild(square);
+        gridContainer.appendChild(square);
     }
+}
+
+createGrid(16);
+
+document.body.addEventListener('mouseover', function(e) {
+    if (e.target.className == "square") {
+        e.target.classList.add("mouseover");
+    }
+
+});
+
+//GRID ADJUSTMENT
+
+const gridBtn = document.querySelector(".grid-btn");
+gridBtn.addEventListener('click', setNewGrid);
+
+function setNewGrid() {
+    const size = prompt("How many squares on each side?");
+    //createGrid(size,size);
 }
